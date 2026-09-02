89059800131a279f39fa21d8949a759a2ed31e44
kui-2026 <2799954285@qq.com>
2026-09-02 10:47:38 +0800

    Load local .env safely on server

--- src/local-panel.js
+++ src/local-panel.js
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { createServer } from 'node:http';
 import { NeteaseClient } from './netease.js';
 import { ListenTogetherSessionManager } from './session-manager.js';
