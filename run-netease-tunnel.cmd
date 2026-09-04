13ba2f1f0578dadbc75144d17c102c8a5b2f9282
kui-2026 <2799954285@qq.com>
2026-09-04 22:18:11 +0800

    fix: add persistent NetEase tunnel launcher

---
+++ run-netease-tunnel.cmd
@@ -0,0 +1,3 @@
+@echo off
+cd /d "C:\tunnel-client"
+"C:\tunnel-client\tunnel-client.exe" run --profile netease >> "C:\tunnel-client\netease-tunnel.log" 2>> "C:\tunnel-client\netease-tunnel-error.log"
